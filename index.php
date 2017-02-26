79bb59742d6694478cd0c24c5703fb10707cc561
srikanth <srikanthkanjarla@gmail.com>
2017-02-26 12:34:55 +0530

    portfolio site first commit to github

---
+++ index.php
@@ -0,0 +1,191 @@
+
+  <!DOCTYPE html>
+<html  lang="US-en">
+ 
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Srikanth Kanjarla</title>
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+    <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Fira+Sans|Lato|Slabo+27px|Pangolin|Bad+Script|Rancho|Tangerine" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="css/portfolio.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    
+
+</head>
+
+<body> 
+    <div id="wrapper">
+        <div class="container-fluid">
+
+            <header class="" role="banner">
+
+                <nav class="navbar navbar-default navbar-fixed-top" id="site-nav">
+                    <div class="container-fluid">
+                        <!-- brand and toggle better grouped together for better display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#portfolio-menu" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                    </button>
+                            <a class="navbar-brand scrollable" id="logo" href="#profile">S</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="portfolio-menu">
+                            <ul class="nav navbar-nav navbar-right" id="main-nav">
+                                <li><a href="#profile" class="scrollable">about</a></li>
+                                <li><a href="#portfolio" class="scrollable">portfolio</a></li>
+                                <li><a href="#contact" class="scrollable">contact</a></li>
+                                <!-- <li><a href="">RECENT WORK</a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+
+                </nav>
+            </header>
+            <!-- end of header -->
+
+            <!-- profile section  -->
+            <section id="profile">
+                <div class="container-fluid">
+                    <br />
+                    <br />
+                    <h1 class="profile-title">srikanth k </h1>
+                    <br />
+
+                    <h2 class="profile-description">Front-End Developer and UX/UI designer, with practical experience in project management, and creative direction.</h2>
+                   <p class ="check-profile-container"> <a class="btn btn-info btn-check-profile scrollable" href="#portfolio">CHECK MY PROFILE</a></p>
+                </div>
+                <br /><br /><br /><br /><br />
+                
+                 
+            </section>
+            <section class="profile-transparent-overlay">
+
+            </section>
+            <!-- end of profile -->
+
+            <!-- portfolio  -->
+            <section id="portfolio">
+                 
+                    <h1>PORTFOLIO</h1>
+                    <hr />
+                    <div class="portfolio-grid">
+                        <div class="img-container">
+
+                            <a href="#" class="img-link" targer="_blank">
+                                <figure class="img-portfolio"><img class="img-responsive" src="/assets/images/nayan3.jpg" />
+                                    <figcaption class="img-caption"><span class="caption-title">some text here</span></figcaption>
+                                </figure>
+                            </a>
+
+                            <a href="#" class="img-link" targer="_blank">
+                                <figure class="img-portfolio"><img class="img-responsive" src="/assets/images/nayan4.jpg" />
+                                    <figcaption class="img-caption"><span class="caption-title">some text here</span></figcaption>
+                                </figure>
+                            </a>
+
+                            <a href="#" class="img-link" targer="_blank">
+                                <figure class="img-portfolio"><img class="img-responsive" src="/assets/images/nayan3.jpg" />
+                                    <figcaption class="img-caption"><span class="caption-title">some text here</span></figcaption>
+                                </figure>
+                            </a>
+
+                             
+
+
+                        </div>
+                    </div>
+
+                
+            </section>
+            <!-- end of portfolio -->
+
+            <div class="clr"></div>
+
+            <!-- contact -->
+            <section id="contact">
+                
+                    <h1>CONTACT ME</h1>
+                    <hr />
+                    <br /><br />
+                    <div class="row">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-4">
+                         <form class="form-horizontal" id="contact-form" novalidate="">
+                            <div class="form-group form-item">
+                                <label for="name" class="control-label">Name</label>
+                                <input type="text" id="name" class="form-control" placeholder="Name" required="" />
+                            </div>
+                            <div class="form-group form-item">
+                                <label for="email" class="control-label">Email Address</label>
+                                <input type="email" class="form-control" id="email" placeholder="Email Address" required="" />
+                            </div>
+                            <div class="form-group form-item">
+                                <label for="message" class="control-label">Message</label>
+                                <textarea rows="2" class="form-control" cols="2" id="message" placeholder="Message" required="" /></textarea>
+                            </div><br/>
+                            <button type="submit" class="btn btn-primary button" id="send-msg-btn" name="send"><span class="fa fa-paper-plane plane"></span> Contact Me</button>
+                         </form>
+                            </div>
+
+                            <div class="col-md-1"></div>
+                            <div class="col-md-4 contact-form-description">
+                            <p>
+                       &#34 Want to get in touch with me? Be it to request more info about myself or my experience, just feel free to drop me a line anytime.&#34
+                    </p>
+                    <br />
+                    <br />
+                            </div>
+                            </div>
+
+                    
+                    
+              
+            </section>
+
+
+            <!-- scroll to top button -->
+            <div class="scroll-top">
+                <a id="scroll" style="display:none" href="#home" class="btn scrollable pull-right"><i class="fa fa-chevron-up"></i></a>
+            </div>
+
+            <div class="clr"></div>
+            <br />
+            <br />
+            <!-- footer content -->
+            <footer id="footer">
+
+                
+                     
+                    <ul>
+                        <li><a class="button social" href="#"><i class="fa fa-fw fa-linkedin"></i></a></li>
+                        <li><a class="button social" href="#"><i class="fa fa-fw fa-github"></i></a></li>
+                        <li><a class="button social" href="#"><i class="fa fa-fw fa-twitter"></i></a></li>
+                        <li><a class="button social" href="#" title="My Flickr"><i class="fa fa-fw fa-flickr"></i></a></li>
+                    </ul>
+                </div>
+
+                 
+            </footer>
+        </div>
+    </div>
+     
+        
+
+</body>
+
+</html>
